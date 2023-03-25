67e1f4e19362385e373e2fd14f8728f377783085
Kinyabundi <kinyachristine14@gmail.com>
2023-03-25 15:38:36 +0300

    added create replies

--- contracts/vcloud/lib.rs
+++ contracts/vcloud/lib.rs
@@ -30,6 +30,21 @@ mod vcloud {
         addressed_to: AccountId,
         #[ink(topic)]
         sent_by: AccountId,
+        #[ink(topic)]
+        msg: String,
+    }
+    #[ink(event)]
+    pub struct ReplyCreated {
+        #[ink(topic)]
+        reply_id: u64,
+        #[ink(topic)]
+        request_id: u64,
+        #[ink(topic)]
+        addressed_to: AccountId,
+        #[ink(topic)]
+        sent_by: AccountId,
+        #[ink(topic)]
+        msg: String,
     }
 
     #[derive(scale::Encode, scale::Decode, Debug, PartialEq, Eq, Clone)]
@@ -78,12 +93,44 @@ mod vcloud {
         }
     }
 
+    #[derive(scale::Encode, scale::Decode, Debug, PartialEq, Eq, Clone)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct Reply {
+        msg: String,
+        addressed_to: AccountId,
+        sent_by: AccountId,
+        sent_at: Timestamp,
+        request_id: u64,
+        document_name: String,
+        document_cid: String,
+        reply_id: u64,
+    }
+
+    impl Default for Reply {
+        fn default() -> Self {
+            Self {
+                msg: String::from(""),
+                addressed_to: AccountId::from([0x0; 32]),
+                sent_by: AccountId::from([0x0; 32]),
+                sent_at: Timestamp::default(),
+                request_id: 0,
+                document_name: String::from(""),
+                document_cid: String::from(""),
+            }
+        }
+    }
+
     #[ink(storage)]
     pub struct Vcloud {
         users: Mapping<AccountId, User>,
         users_items: Vec<AccountId>,
         requests: Mapping<u64, Request>,
         requests_items: Vec<u64>,
+        replies: Mapping<u64, Reply>,
+        replies_items: Vec<u64>,
     }
 
     impl Vcloud {
@@ -94,6 +141,8 @@ mod vcloud {
                 users_items: Vec::new(),
                 requests: Mapping::new(),
                 requests_items: Vec::new(),
+                replies: Mapping::new(),
+                replies_items: Vec::new(),
             }
         }
         /// add new user
@@ -113,13 +162,22 @@ mod vcloud {
             self.env().emit_event(UserCreated { user: caller });
             Ok(())
         }
-        //get user
+        /// get user
         #[ink(message)]
         pub fn get_user(&self, user: AccountId) -> Option<User> {
             if !self.users.contains(&user) {
                 return None;
             }
             Some(self.users.get(&user).unwrap())
+        }
+        /// get all users
+        #[ink(message)]
+        pub fn get_users(&self) -> Vec<User> {
+            let mut users = Vec::new();
+            for user in self.users_items.iter() {
+                users.push(self.users.get(user).unwrap());
+            }
+            users
         }
         /// create new document request
         #[ink(message)]
@@ -143,8 +201,130 @@ mod vcloud {
                 request_id,
                 addressed_to,
                 sent_by: caller.clone(),
+                msg,
             });
             Ok(())
         }
+        ///get request
+        #[ink(message)]
+        pub fn get_request(&self, request_id: u64) -> Request {
+            if !self.requests.contains(&request_id) {
+                return None;
+            }
+            Some(self.requests.get(&request_id).unwrap())
+        }
+        /// get all requests
+        #[ink(message)]
+        pub fn get_requests(&self) -> Vec<Request> {
+            let mut requests = Vec::new();
+            for request_id in self.requests_items.iter() {
+                let request = self.requests.get(request_id).unwrap();
+                requests.push(request);
+            }
+            requests
+        }
+
+        /// get all requests by sent_by
+        #[ink(message)]
+        pub fn get_request_by_sent_by(&self, sent_by: AccountId) -> Vec<Request> {
+            let mut requests = Vec::new();
+            for request_id in self.requests_items.iter() {
+                let request = self.requests.get(request_id).unwrap();
+                if request.addressed_to == user {
+                    requests.push(request);
+                }
+            }
+            requests
+        }
+
+        /// get all requests by addressed_to
+        #[ink(message)]
+        pub fn get_request_by_addressed_to(&self, addressed_to: AccountId) -> Vec<Request> {
+            let mut requests = Vec::new();
+            for request_id in self.requests_items.iter() {
+                let request = self.requests.get(request_id).unwrap();
+                if request.addressed_to == user {
+                    requests.push(request);
+                }
+            }
+            requests
+        }
+
+        /// create new document reply
+        #[ink(message)]
+        pub fn create_reply(
+            &mut self,
+            msg: String,
+            document_name: String,
+            document_cid: String,
+            request_id: u64,
+            reply_id: u64,
+        ) -> Result<()> {
+            let caller = self.env().caller();
+            let reply = Reply {
+                msg,
+                addressed_to: self.requests.get(&request_id).unwrap().addressed_to,
+                sent_by: caller,
+                sent_at: self.env().block_timestamp(),
+                request_id: request_id.clone(),
+                document_name,
+                document_cid,
+                reply_id,
+            };
+            self.replies.insert(reply.reply_id, &reply);
+            self.replies_items.push(reply.reply_id);
+            self.env().emit_event(ReplyCreated {
+                reply_id,
+                request_id,
+                addressed_to: caller.clone(),
+                sent_by: caller.clone(),
+                msg,
+
+            });
+            Ok(())
+        }
+        ///get reply
+        #[ink(message)]
+        pub fn get_reply(&self, reply_id: u64) -> Reply {
+            if !self.replies.contains(&reply_id) {
+                return None;
+            }
+            Some(self.replies.get(&reply_id).unwrap())
+        }
+        /// get all replies
+        #[ink(message)]
+        pub fn get_all_replies(&self) -> Vec<Reply> {
+            let mut replies = Vec::new();
+            for reply_id in self.replies_items.iter() {
+                let reply = self.replies.get(reply_id).unwrap();
+                replies.push(reply);
+            }
+            replies
+        }
+        /// get all replies by sent_by
+        #[ink(message)]
+        pub fn get_reply_by_sent_by(&self, sent_by: AccountId) -> Vec<Reply> {
+            let mut replies = Vec::new();
+            for reply_id in self.replies_items.iter() {
+                let reply = self.replies.get(reply_id).unwrap();
+                if reply.sent_by == user {
+                    replies.push(reply);
+                }
+            }
+            replies
+        }
+        /// get all replies by addressed_to
+        #[ink(message)]
+        pub fn get_reply_by_addressed_to(&self, addressed_to: AccountId) -> Vec<Reply> {
+            let mut replies = Vec::new();
+            for reply_id in self.replies_items.iter() {
+                let reply = self.replies.get(reply_id).unwrap();
+                if reply.addressed_to == user {
+                    replies.push(reply);
+                }
+            }
+            replies
+        }
+
     }
 }
