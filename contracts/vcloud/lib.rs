10ef6b7a0b49a3e6a410eefb84c8964e0997cfd7
Kinyabundi <kinyachristine14@gmail.com>
2023-03-25 14:24:58 +0300

    added  create user and send request contract

--- contracts/vcloud/lib.rs
+++ contracts/vcloud/lib.rs
@@ -2,141 +2,143 @@
 
 #[ink::contract]
 mod vcloud {
+    use ink::prelude::string::String;
+    use ink::storage::Mapping;
+    use ink_prelude::vec::Vec;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    /// Errors that might occur while executing the contract.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// The user is already exists.
+        UserAlreadyExists,
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
+    
+    /// Events that are emitted after calls.
+    #[ink(event)]
+    pub struct UserCreated {
+        #[ink(topic)]
+        user: AccountId,
+    }
+    #[ink(event)]
+    pub struct RequestCreated {
+        #[ink(topic)]
+        request_id: u64,
+        #[ink(topic)]
+        addressed_to: AccountId,
+        #[ink(topic)]
+        sent_by: AccountId,
+    }
+
+    #[derive(scale::Encode, scale::Decode, Debug, PartialEq, Eq, Copy, Clone)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink_storage::traits::StorageLayout))]
+    pub struct User {
+         email: String,
+         phone: String,
+         address: AccountId,
+    }
+
+    impl Default for User {
+        fn default() -> Self {
+            Self {
+                email: String::from(""),
+                phone: String::from(""),
+                address: AccountId::from([0x0; 32]),
+            }
+        }
+    }
+
+    #[derive(scale::Encode, scale::Decode, Debug, PartialEq, Eq, Copy, Clone)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink_storage::traits::StorageLayout))]
+    pub struct Request {
+      msg: String,
+      addressed_to: AccountId,
+      sent_by: AccountId,
+      sent_at: Timestamp,
+      request_id: u64,
+    }
+
+    impl Default for Request {
+        fn default() -> Self {
+            Self {
+                msg: String::from(""),
+                addressed_to: AccountId::from([0x0; 32]),
+                sent_by: AccountId::from([0x0; 32]),
+                sent_at: Timestamp::default(),
+                request_id: 0,
+            }
+        }
+    }
+
     #[ink(storage)]
     pub struct Vcloud {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        users: Mapping<AccountId, User>,
+        users_items: Vec<AccountId>,
+        requests: Mapping<u64, Request>,
+        requests_items: Vec<u64>,
     }
 
     impl Vcloud {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {
+                users: Mapping::new(),
+                users_items: Vec::new(),
+                requests: Mapping::new(),
+                requests_items: Vec::new(),
+            }
         }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// add new user
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn add_user(&mut self, email: String, phone: String) {
+            let caller = self.env().caller();
+            let user = User {
+                email,
+                phone,
+                address: caller,
+            };
+            if self.users.contains($caller) {
+                return Err(Error::UserAlreadyExists);
+            }
+            self.users.insert(caller, &user);
+            self.users_items.push(caller);
+            self.env().emit_event(UserCreated { user: caller });
+                Ok(())
         }
-
-        /// Simply returns the current value of our `bool`.
+        //get user
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn get_user(&self, user: AccountId) -> User {
+            if !self.users.contains(&user) {
+                return None;
+            }
+            Some(self.users.get(&user).unwrap())
         }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let vcloud = Vcloud::default();
-            assert_eq!(vcloud.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut vcloud = Vcloud::new(false);
-            assert_eq!(vcloud.get(), false);
-            vcloud.flip();
-            assert_eq!(vcloud.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = VcloudRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("vcloud", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<VcloudRef>(contract_account_id.clone())
-                .call(|vcloud| vcloud.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = VcloudRef::new(false);
-            let contract_account_id = client
-                .instantiate("vcloud", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<VcloudRef>(contract_account_id.clone())
-                .call(|vcloud| vcloud.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<VcloudRef>(contract_account_id.clone())
-                .call(|vcloud| vcloud.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<VcloudRef>(contract_account_id.clone())
-                .call(|vcloud| vcloud.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
+        /// create new document request
+        #[ink(message)]
+        pub fn create_request(&mut self, msg: String, addressed_to: AccountId) {
+            let caller = self.env().caller();
+            let request = Request {
+                msg,
+                addressed_to,
+                sent_by: caller,
+                sent_at: self.env().block_timestamp(),
+                request_id,
+            };
+            self.requests.insert(request.request_id, &request);
+            self.requests_items.push(request.request_id);
+            self.env().emit_event(RequestCreated {
+            request_id,
+            addressed_to,
+            sent_by,
+            });
             Ok(())
         }
     }
 }
+
+    
+
+    
+
